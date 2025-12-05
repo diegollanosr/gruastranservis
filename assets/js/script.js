5782e9ac775683f6617a6de011b03aa54f7b685b
diegollanosr <dllanosr17@gmail.com>
2025-12-05 11:05:02 -0500

    modo oscuro y optimizacion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,50 @@
 const $anhoActual = document.getElementById('anhoActual');
 const $body = document.getElementById('body');
 const $listaNav = document.querySelectorAll('.nav_ul a[href^="#"]');
+const $btnDarkMode = document.getElementById('botonModoOscuro');
 
 /* cambio de visualizacion*/
 window.addEventListener('scroll', ()=>{
 	$body.classList.toggle('scroll', window.scrollY > 20)
 })
+
+const temaGuardado = localStorage.getItem("theme");
+
+/* modo oscuro*/
+const modClaro =()=>{
+	localStorage.setItem("theme", "light")
+	$btnDarkMode.classList.add("fa-regular")
+	$btnDarkMode.classList.add("fa-moon")
+	$btnDarkMode.classList.remove("fa-solid")
+	$btnDarkMode.classList.remove("fa-sun")
+	$body.classList.remove("dark")
+}
+const modOscuro =()=>{
+	localStorage.setItem("theme", "dark")
+	$btnDarkMode.classList.remove("fa-regular")
+	$btnDarkMode.classList.remove("fa-moon")
+	$btnDarkMode.classList.add("fa-solid")
+	$btnDarkMode.classList.add("fa-sun")
+	$body.classList.add("dark")
+}
+
+// Recordar preferencia de modo
+if (temaGuardado === "dark") {
+  modOscuro();
+} else if (temaGuardado === "light") {
+  modClaro();
+} else {
+  // Si no hay preferencia guardada, usar la del sistema
+  if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    modOscuro();
+  } else {
+    modClaro();
+  }
+}
+// Escuchar cambios del sistema
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {e.matches ? modOscuro() : modClaro();});
+// Alternar manualmente
+$btnDarkMode.addEventListener('click', ()=> {if($body.classList.contains("dark")){modClaro()}else{ modOscuro()}})
 
 // //Actualizar Año
 $anhoActual.innerHTML = new Date().getFullYear();
